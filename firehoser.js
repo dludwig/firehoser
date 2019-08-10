36bc5fa5966256e3a7a9561b74e7a4464b425478
dludwig <321330+dludwig@users.noreply.github.com>
2019-08-09 17:38:41 -0700

    switch from jayschema to ajv as ajv is still maintained

--- firehoser.js
+++ firehoser.js
@@ -3,39 +3,43 @@
 var _ = require('lodash');
 var AWS = require('aws-sdk');
 var async = require('async');
-var JaySchema = require('jayschema');
 var moment = require('moment');
 
-var schemaValidator = new JaySchema();
+var Ajv = require('ajv');
+var ajv = new Ajv();
 
-class DeliveryStream{
-    constructor(name, awsConfig=null, schema=null, retryInterval=1500, firehose=null, logger=null){
+class DeliveryStream {
+    constructor(name, awsConfig = null, schema = null, retryInterval = 1500, firehose = null, logger = null) {
         this.maxIngestion = 400;
         this.maxDrains = 3;
         this.maxRetries = 40;
         this.name = name;
-        if (awsConfig !== null){
+        if (awsConfig !== null) {
             AWS.config.update(awsConfig);
         }
         this.schema = schema;
         this.retryInterval = retryInterval;
-        this.firehose = firehose ? firehose : new AWS.Firehose({params: {DeliveryStreamName: name}});
+        this.firehose = firehose ? firehose : new AWS.Firehose({
+            params: {
+                DeliveryStreamName: name
+            }
+        });
         this.log = logger ? logger : () => {};
     }
 
-    validateRecord(record){
-        return schemaValidator.validate(record, this.schema);
+    validateRecord(record) {
+        return ajv.validate(this.schema, record);
     }
 
-    validateRecords(records){
-        if (!this.schema){
+    validateRecords(records) {
+        if (!this.schema) {
             return [records, []];
         }
         let validRecords = [];
         let invalidRecords = [];
         _.forEach(records, (record) => {
             let validationErrors = this.validateRecord(record);
-            if (_.isEmpty(validationErrors)){
+            if (_.isEmpty(validationErrors)) {
                 validRecords.push(record);
             } else {
                 let ve = validationErrors[0];
@@ -50,15 +54,17 @@ class DeliveryStream{
         return [validRecords, invalidRecords];
     }
 
-    formatRecord(record){
-        return {Data: record + '\n'};
+    formatRecord(record) {
+        return {
+            Data: record + '\n'
+        };
     }
 
-    putRecord(record){
+    putRecord(record) {
         return this.putRecords([record]);
     }
 
-    putRecords(records){
+    putRecords(records) {
         var self = this;
         this.log(`DeliveryStream.putRecords() called with ${records.length} records.`);
         return new Promise((resolve, reject) => {
@@ -69,15 +75,15 @@ class DeliveryStream{
             records = _.map(validRecords, this.formatRecord);
             let chunks = _.chunk(records, this.maxIngestion);
             let tasks = [];
-            for (let i=0; i < chunks.length; i++){
+            for (let i = 0; i < chunks.length; i++) {
                 tasks.push(this.drain.bind(this, chunks[i]));
             }
 
             // Schedule the chunks all at the same time.
             self.log(`Kicking off ${tasks.length} calls to drain() for ${records.length} records.`);
-            async.parallelLimit(tasks, this.maxDrains, function(err, results){
+            async.parallelLimit(tasks, this.maxDrains, function (err, results) {
                 let allErrors = invalidRecords.concat(_.flatten(results));
-                if (err || !_.isEmpty(allErrors)){
+                if (err || !_.isEmpty(allErrors)) {
                     return reject(allErrors);
                 }
                 return resolve();
@@ -85,13 +91,15 @@ class DeliveryStream{
         });
     }
 
-    drain(records, cb, numRetries=0){
+    drain(records, cb, numRetries = 0) {
         var leftovers = [];
         // var self = this;
         this.log(`Draining ${records.length} records.  Pass #${numRetries + 1}`);
-        this.firehose.putRecordBatch({Records: records}, (firehoseErr, resp)=>{
+        this.firehose.putRecordBatch({
+            Records: records
+        }, (firehoseErr, resp) => {
             // Stuff broke!
-            if (firehoseErr){
+            if (firehoseErr) {
                 return cb(null, {
                     type: "firehose",
                     description: "Internal aws-sdk error.",
@@ -101,13 +109,12 @@ class DeliveryStream{
             }
 
             // Not all records make it in, but firehose keeps on chugging!
-            if (resp.FailedPutCount > 0){
-            }
+            if (resp.FailedPutCount > 0) {}
 
             // Push errored records back into the next list.
-            for (let [orig, result] of _.zip(records, resp.RequestResponses)){
-                if (!_.isUndefined(result.ErrorCode)){
-                    this.log(`Got ErrorCode ${result.ErrorCode} for record ${orig}`,'error');
+            for (let [orig, result] of _.zip(records, resp.RequestResponses)) {
+                if (!_.isUndefined(result.ErrorCode)) {
+                    this.log(`Got ErrorCode ${result.ErrorCode} for record ${orig}`, 'error');
                     leftovers.push({
                         type: "firehose",
                         description: result.ErrorMessage,
@@ -121,11 +128,11 @@ class DeliveryStream{
             }
 
             // Recurse!
-            if (leftovers.length && numRetries < this.maxRetries){
+            if (leftovers.length && numRetries < this.maxRetries) {
                 // We're about to recurse, let the child handle storing error details.
                 leftovers = _.map(leftovers, pickData);
 
-                return setTimeout(()=>{
+                return setTimeout(() => {
                     this.drain(leftovers, cb, numRetries + 1);
                 }, this.retryInterval);
             } else {
@@ -136,13 +143,13 @@ class DeliveryStream{
 }
 
 class JSONDeliveryStream extends DeliveryStream {
-    formatRecord(record){
+    formatRecord(record) {
         return super.formatRecord(JSON.stringify(record));
     }
 }
 
 class QueueableDeliveryStream extends DeliveryStream {
-    constructor(name, maxTime=30000, maxSize=500, ...args){
+    constructor(name, maxTime = 30000, maxSize = 500, ...args) {
         super(name, ...args);
         this.queue = [];
         this.timeout = null;
@@ -152,17 +159,17 @@ class QueueableDeliveryStream extends DeliveryStream {
         setInterval(this.drainQueue.bind(this), this.maxTime);
     }
 
-    putRecords(records){
+    putRecords(records) {
         this.log(`QueueableDeliveryStream.putRecords() called with ${records.length} records.`);
         this.queue.push(...records);
-        if (this.promise === null){
+        if (this.promise === null) {
             this.promise = new Promise((resolve, reject) => {
                 this.resolver = resolve;
                 this.rejecter = reject;
             });
         }
         this.log(`queue size is: ${this.queue.length}, maxSize is: ${this.maxSize}.`);
-        if (this.queue.length >= this.maxSize){
+        if (this.queue.length >= this.maxSize) {
             // Queue's full!
             this.log(`queue is full, draining immediately.`);
             setImmediate(this.drainQueue.bind(this));
@@ -170,11 +177,11 @@ class QueueableDeliveryStream extends DeliveryStream {
         return this.promise;
     }
 
-    drainQueue(){
+    drainQueue() {
         this.log(`Countdown timer expired or queue limit reached.`);
         this.log(`Time to drain the queue of ${this.queue.length} records.`);
         let toQueue = this.queue.splice(0, this.queue.length);
-        if (!toQueue.length){
+        if (!toQueue.length) {
             this.log(`No records in queue, not draining anything.`);
             return;
         }
@@ -187,19 +194,21 @@ class QueueableDeliveryStream extends DeliveryStream {
 }
 
 class QueueableJSONDeliveryStream extends QueueableDeliveryStream {
-    formatRecord(record){
+    formatRecord(record) {
         return super.formatRecord(JSON.stringify(record));
     }
 }
 
 class PlainDeliveryStream extends DeliveryStream {
-    formatRecord(record){
-        return { Data: record };
+    formatRecord(record) {
+        return {
+            Data: record
+        };
     }
 }
 
-function buildSchemaErrorDescription(ve){
-    if (ve.desc){
+function buildSchemaErrorDescription(ve) {
+    if (ve.desc) {
         return ve.desc;
     }
     let field = ve.instanceContext.replace(/(#\/)|(#)/ig, "").replace(/\//g, ".")
@@ -207,12 +216,12 @@ function buildSchemaErrorDescription(ve){
 }
 
 
-function makeRedshiftTimestamp(input){
+function makeRedshiftTimestamp(input) {
     return moment(input).utc().format('YYYY-MM-DD HH:mm:ss')
 }
 
-function pickData(leftover){
-  return _.pick(leftover.originalRecord, ['Data']);
+function pickData(leftover) {
+    return _.pick(leftover.originalRecord, ['Data']);
 }
 
 module.exports = {
